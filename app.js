5030573230e5ad8fe219767ecf56002820570aef
Cyril Gouverneur <cyril@imac-de-cyril.home>
2021-06-09 15:13:26 +0200

    init project

---
+++ app.js
@@ -0,0 +1,54 @@
+const main = document.getElementById('main')
+const addUserBtn = document.getElementById('add-user')
+const doubleBtn = document.getElementById('double')
+const showMillionairesBtn = document.getElementById('show-millionaires')
+const sortBtn = document.getElementById('sort')
+const calculateWealthBtn = document.getElementById('calculate-wealth')
+
+let data = []
+
+getRandomUser()
+getRandomUser()
+getRandomUser()
+
+// fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api')
+    const data = await res.json()
+
+    const user = data.results[0]
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+    addData(newUser)
+}
+
+function addData(obj) {
+    data.push(obj)
+
+    updateDOM()
+}
+
+// Update DOM
+function updateDOM(providedData = data) {
+    main.innerHTML = `<h2><strong>Person</strong> Wealth</h2>`
+
+    providedData.forEach(person => {
+        const element = document.createElement('div')
+        element.classList.add('person')
+        element.innerHTML = `<strong>${person.name}</strong> ${formatMoney(person.money)}`
+        main.appendChild(element)
+    })
+}
+
+// Format number as money
+function formatMoney(num) {
+    return '$' + num.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+}
+
+
+// Event listener
+addUserBtn.addEventListener('click', getRandomUser)
